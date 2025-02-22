19b4400bff56f00edf215c7eb1af52116adec789
KRISHNA <kraggr2909@gmail.com>
2025-02-22 23:54:33 +0530

    bd

---
+++ index.js
@@ -0,0 +1,18 @@
+//react starting.....
+/*
+     blundlers - combines multiple js file into a single manageable file like vite ,parcel etc...
+    
+     to create a react app:-
+     1> npm (node package manager) - allows us to install library
+     2> npx (node package executor) - when we dont want to install but directly execute the file
+
+     npx create-react-app <name of app> - takes time...
+     alternative is vite and parcel etc....
+
+     scripts - project ko chalane ke liye ya production ke liye ready krna 
+     build - generates html css and js for browser
+     eject - allows us to eject our app from react 
+
+     start script - to run our react project in development env.
+     // npm run start <enter>
+*/
