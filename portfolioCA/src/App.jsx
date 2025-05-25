3869a30ea3e162a544d050f901c2abdd2cb5c6d0
KRISHNA <kraggr2909@gmail.com>
2025-05-25 20:12:19 +0530

    portfolio-karan

---
+++ portfolioCA/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-4xl font-extrabold text-center p-4 animate-pulse'>KARAN PORTFOLIO</h1>
+    </>
+  )
+}
+
+export default App
